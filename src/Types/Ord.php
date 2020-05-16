88aa161bb8d24daba83246e89d7c13760c2e6efc
James Dunne <james@thefreshuk.com>
2020-05-16 17:31:24 +0100

    feat: implement Ord interface

---
+++ src/Types/Ord.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Fun\Types;
+
+/**
+ * @template T
+ * @psalm-immutable
+ * @package Fun\Types
+ */
+interface Ord extends Eq
+{
+    public const LT = 'lt';
+    public const GT = 'lt';
+    public const EQ = 'eq';
+
+    /**
+     * Returns static::LT, static::GT or static::EQ
+     *
+     * @param Ord<T> $x
+     * @return Ord::LT | Ord::GT | Ord::EQ
+     */
+    public function compare(Ord $x);
+
+    /**
+     * True if $this < $x.
+     *
+     * @param Ord<T> $x
+     * @return bool
+     */
+    public function lt($x): bool;
+
+    /**
+     * True if $this <= $x.
+     *
+     * @param Ord<T> $x
+     * @return bool
+     */
+    public function lte($x): bool;
+
+    /**
+     * True if $this > $x.
+     *
+     * @param Ord<T> $x
+     * @return bool
+     */
+    public function gt($x): bool;
+
+    /**
+     * True if $this >= $x.
+     *
+     * @param Ord<T> $x
+     * @return bool
+     */
+    public function gte($x): bool;
+
+    /**
+     * Takes $x of type Ord<T> and returns the smaller of $this or $x.
+     *
+     * @param Ord<T> $x
+     * @return Ord<T>
+     */
+    public function min($x): Ord;
+
+    /**
+     * Takes $x of type T and returns the larger of $this or $x.
+     *
+     * @param Ord<T> $x
+     * @return Ord<T>
+     */
+    public function max($x): Ord;
+}
