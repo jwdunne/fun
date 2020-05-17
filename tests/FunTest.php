1443585e3c01fe59685d68445c4942e081c0aa68
James Dunne <james@thefreshuk.com>
2020-05-17 13:04:45 +0100

    tests: convert accept_triple and accept_tuple to property-based tests

--- tests/FunTest.php
+++ tests/FunTest.php
@@ -3,6 +3,9 @@
 namespace Tests;
 
 use PHPUnit\Framework\TestCase;
+use QuickCheck\Generator as Gen;
+use QuickCheck\PHPUnit\PropertyConstraint;
+use QuickCheck\Property;
 
 use function Fun\accept_triple;
 use function Fun\accept_tuple;
@@ -12,12 +15,24 @@ class FunTest extends TestCase
     public function test_accept_tuple()
     {
         $add = accept_tuple(fn ($x, $y) => $x + $y);
-        $this->assertEquals(2, $add([1, 1]));
+        $this->assertThat(
+            Property::forAll(
+                [Gen::tuples(Gen::ints(), Gen::ints())],
+                fn ($xs) => $add($xs) === $xs[0] + $xs[1]
+            ),
+            PropertyConstraint::check()
+        );
     }
 
     public function test_accept_triple()
     {
         $add3 = accept_triple(fn ($x, $y, $z) => $x + $y + $z);
-        $this->assertEquals(6, $add3([1, 2, 3]));
+        $this->assertThat(
+            Property::forAll(
+                [Gen::tuples(Gen::ints(), Gen::ints(), Gen::ints())],
+                fn ($xs) => $add3($xs) === $xs[0] + $xs[1] + $xs[2]
+            ),
+            PropertyConstraint::check()
+        );
     }
 }
