b46b8e9865b3e5e30662c8c4a7bba04af7bee01c
James Dunne <james@thefreshuk.com>
2020-05-17 14:07:47 +0100

    feat: stub out ord binary relations

--- src/Relations.php
+++ src/Relations.php
@@ -12,6 +12,7 @@
 namespace Fun;
 
 use Fun\Types\Eq;
+use Fun\Types\Ord;
 
 /**
  * @const callable
@@ -55,4 +56,142 @@ const neq = '\Fun\neq';
 function neq($a, $b)
 {
     return !eq($a, $b);
+}
+
+/**
+ * @const callable
+ */
+const lt = '\Fun\lt';
+
+/**
+ * Less-than binary relation.
+ *
+ * @template T
+ * @psalm-pure
+ * @param T $a
+ * @param T $b
+ * @return bool
+ */
+function lt($a, $b): bool
+{
+    return $a < $b;
+}
+
+/**
+ * @const callable
+ */
+const lte = '\Fun\lte';
+
+/**
+ * Less-than-or-equal to binary relation.
+ *
+ * @template T
+ * @psalm-pure
+ * @param T $a
+ * @param T $b
+ * @return bool
+ */
+function lte($a, $b): bool
+{
+
+    return eq($a, $b) || lt($a, $b);
+}
+
+/**
+ * @const callable
+ */
+const gt = '\Fun\gt';
+
+/**
+ * Greater-than binary relation.
+ *
+ * @template T
+ * @psalm-pure
+ * @param T $a
+ * @param T $b
+ * @return bool
+ */
+function gt($a, $b): bool
+{
+    return $a > $b;
+}
+
+/**
+ * @const callable
+ */
+const gte = '\Fun\gte';
+
+/**
+ * Greater-than-or-equal-to binary relation.
+ *
+ * @template T
+ * @psalm-pure
+ * @param T $a
+ * @param T $b
+ * @return bool
+ */
+function gte($a, $b): bool
+{
+    return eq($a, $b) || gt($a, $b);
+}
+
+/**
+ * @const callable
+ */
+const min = '\Fun\min';
+
+/**
+ * Takes arguments $a, $b and returns the minimum of the two.
+ *
+ * @template T
+ * @psalm-pure
+ * @param T $a
+ * @param T $b
+ * @return T
+ */
+function min($a, $b)
+{
+    return lt($a, $b) ? $a : $b;
+}
+
+/**
+ * @const callable
+ */
+const max = '\Fun\max';
+
+/**
+ * Takes arguments $a, $b and returns the maximum of the two.
+ *
+ * @template T
+ * @psalm-pure
+ * @param T $a
+ * @param T $b
+ * @return T
+ */
+function max($a, $b)
+{
+    return gt($a, $b) ? $a : $b;
+}
+
+/**
+ * @const callable
+ */
+const compare = '\Fun\compare';
+
+/**
+ * Takes argument $a, $b and returns Ord::GT, Ord::LT or Ord::EQ.
+ *
+ * @template T
+ * @psalm-pure
+ * @param T $a
+ * @param T $b
+ * @return Ord::GT | Ord::LT | Ord::EQ
+ */
+function compare($a, $b)
+{
+    if (eq($a, $b)) {
+        return Ord::GT;
+    }
+
+    return lt($a, $b) ? Ord::LT : Ord::GT;
 }
