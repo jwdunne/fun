2362696a4af5913b5be010136a8f7eeaf3177273
James Dunne <james@thefreshuk.com>
2020-11-23 09:54:11 +0000

    build: stub out string package
    This commit stubs out the string package.
    
    Closes #19

---
+++ src/Str.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * A collection of string utilities.
+ *
+ * @package Fun\Str
+ */
+
+declare(strict_types=1);
+
+namespace Fun\Str;
+
+function contains()
+{
+}
+
+function replace()
+{
+}
+
+function split()
+{
+}
+
+function match()
+{
+}
+
+function test()
+{
+}
